f313911bacff5b7ce81a8d12abe89cafaf88bd21
Anthony61176 <Anthony61176@GB-LD1LPW10G020.mastek.com>
2019-07-16 16:16:54 +0100

    Projected has been created

--- sporttournament/src/main/java/com/mastek/training/sport/sporttournament/SporttournamentApplication.java
+++ sporttournament/src/main/java/com/mastek/training/sport/sporttournament/SporttournamentApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SporttournamentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SporttournamentApplication.class, args);
-	}
+	
 
 }
---
+++ sporttournament1/src/main/java/com/mastek/training/sport/sporttournament1/Sporttournament1Application.java
@@ -0,0 +1,13 @@
+package com.mastek.training.sport.sporttournament1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sporttournament1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sporttournament1Application.class, args);
+	}
+
+}
