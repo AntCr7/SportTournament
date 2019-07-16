d5900dcdc80ee1d5690b8651c489369cbc0e7762
Anthony61176 <Anthony61176@GB-LD1LPW10G020.mastek.com>
2019-07-16 16:02:12 +0100

    1st try

--- sporttournament/src/main/java/com/mastek/training/sport/sporttournament/SporttournamentApplication.java
+++ sporttournament/src/main/java/com/mastek/training/sport/sporttournament/SporttournamentApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SporttournamentApplication {
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		SpringApplication.run(SporttournamentApplication.class, args);
 	}
 
