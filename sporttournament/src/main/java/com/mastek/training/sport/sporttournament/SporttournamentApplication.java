769837083185c165a70a5feb0db04a2a42663f19
Anthony61176 <Anthony61176@GB-LD1LPW10G020.mastek.com>
2019-07-16 15:59:42 +0100

    1st Attempt

---
+++ sporttournament/src/main/java/com/mastek/training/sport/sporttournament/SporttournamentApplication.java
@@ -0,0 +1,13 @@
+package com.mastek.training.sport.sporttournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporttournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporttournamentApplication.class, args);
+	}
+
+}
---
+++ sporttournament/src/test/java/com/mastek/training/sport/sporttournament/SporttournamentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mastek.training.sport.sporttournament;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SporttournamentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
